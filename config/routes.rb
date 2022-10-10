a447030d876d7646f222e3efffb5f6c2b8139e3a
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-10 18:26:30 -0300

    Added links on home page to sign_up and log_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "groups#index"
+  root "home#index"
   devise_for :users
   resources :users
   resources :budgets
   resources :groups
+  resources :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
