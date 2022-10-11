9710b241294ac43e161af4eca765c7c31ecf0bb3
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-11 13:27:58 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  resources :users
-  resources :budgets
-  resources :groups
-  resources :home
+  resources :groups do
+    resources :budgets
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
