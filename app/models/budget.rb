1751d9245fe009eb59f2189cb1b787f9e34133c1
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-12 12:04:18 -0300

    Added logic to create a transaction

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,5 +1,5 @@
 class Budget < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
   has_and_belongs_to_many :groups
 
   validates :name, presence: true, length: { minimum: 1, maximum: 40 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many :groups, foreign_key: 'user_id', dependent: :destroy
+  has_many :groups, dependent: :destroy
   has_many :budgets, foreign_key: 'user_id', dependent: :destroy
 
   validates :name, presence: true
