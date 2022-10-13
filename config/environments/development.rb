1207ed80cc573a7f55ff42690cada36a4989a09c
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-13 12:57:20 -0300

    fix spec

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
---
+++ spec/features/group_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Testing Groups:', type: :feature do
+  before do
+    @user = User.create(
+      name: 'Tester',
+      email: 'test@gmail.com',
+      password: '123456'
+    )
+
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    click_button('Log in')
+    
+    @group = Group.create(
+      name: 'Savings',
+      user_id: @user.id
+    )
+    describe 'GET /' do
+
+      it 'returns http success' do
+        get '/groups'
+        expect(response).to have_http_status(:success)
+      end
+  
+      it 'renders the correct template' do
+        get '/groups'
+        expect(response).to render_template(:index)
+      end
+  
+      it 'includes the name of the app' do
+        get '/groups'
+        expect(response.body).to include('<h1>CATEGORIES</h1>')
+      end
+    end
+  end
+end
