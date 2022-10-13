da45b9db954f14187e828db12cd0e4ab26e233fb
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-10-13 13:32:01 -0300

    fix linter errors

--- spec/features/group_spec.rb
+++ spec/features/group_spec.rb
@@ -12,23 +12,22 @@ RSpec.describe 'Testing Groups:', type: :feature do
     fill_in 'user_email', with: @user.email
     fill_in 'user_password', with: @user.password
     click_button('Log in')
-    
+
     @group = Group.create(
       name: 'Savings',
       user_id: @user.id
     )
     describe 'GET /' do
-
       it 'returns http success' do
         get '/groups'
         expect(response).to have_http_status(:success)
       end
-  
+
       it 'renders the correct template' do
         get '/groups'
         expect(response).to render_template(:index)
       end
-  
+
       it 'includes the name of the app' do
         get '/groups'
         expect(response.body).to include('<h1>CATEGORIES</h1>')
